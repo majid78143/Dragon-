0675e44f2e0b78879e8dd05be2cd3647a03ae61c
Majid <mdmajidansari33640@gmail.com>
2026-08-07 08:24:47 +0530

    Add files via upload

---
+++ static/firebase.js
@@ -0,0 +1,11 @@
+// Dragon Vault Firebase web configuration. Server credentials stay in environment variables.
+const firebaseConfig = {
+  apiKey: "AIzaSyCfoSnzXoIHLXtJlk9iRZuPWtz6nvqEmlA",
+  authDomain: "connecto-5814d.firebaseapp.com",
+  databaseURL: "https://connecto-5814d-default-rtdb.firebaseio.com",
+  projectId: "connecto-5814d",
+  storageBucket: "connecto-5814d.firebasestorage.app",
+  messagingSenderId: "652116709212",
+  appId: "1:652116709212:web:2088b666583a37ae75bb99",
+  measurementId: "G-C5Q9EFLCGV"
+};
